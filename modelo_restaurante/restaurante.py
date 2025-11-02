fa6220342f2ff0edd286e7ed7cb0bc283fe35f02
Alexandra Rocha <alexandrasilvarocha2824@gmail.com>
2025-11-02 16:29:16 -0300

    Atualização do código em função dos exercícios

--- modelo_restaurante/restaurante.py
+++ modelo_restaurante/restaurante.py
@@ -1,59 +1,64 @@
 class Restaurante:
-    nome = ""
-    categoria = ""
-    ativo = False
+    def __init__(self, nome,categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        
+    def __str__(self):
+        return f"{self.nome} | {self.categoria}"
     
-restaurante_praca = Restaurante()
-restaurante_praca.nome = "Praça"
-restaurante_praca.categoria = "Italiana" #Exercício 1: Atribua o valor 'Italiana' ao atributo categoria
-# -------------------------
+restaurante_praca = Restaurante("Praça", "Italiana")
+restaurante_pizza = Restaurante ("Pizza Place", "Fast Food")
 
-#Exercício 2: Acesse o valor do atributo nome da instância restaurante_praca 
-print(f"Qual o nome do restaurante_pizza: {restaurante_praca.nome}")
+print(f"Informações do Restaurante: {restaurante_pizza}")
+print(f"Informações do Restaurante: {restaurante_praca}")
+
+class Cliente: 
+    
+    def __init__(self, nome, email,telefone):
+        self.nome = nome 
+        self.email = email 
+        self.telefone = telefone
+        self.membro_vibe = False
+        
+cliente_eliza = Cliente ("Eliza", "eliza.antonucci@gmail.com", 11942900)
+cliente_alex = Cliente ("Alex", "Alex.lima@gmail.com", 1165432)
+cliente_jenny = Cliente ("jennifer", "jennifer.araujo@gmail.com", 1154398)
+    
+print(vars(cliente_eliza))
+#-----------------
+# Modificação de Estado Cliente (ativo)
+cliente_alex.membro_vibe = True
+print(vars(cliente_alex))
+    
+print("----" * 20)
 
 #-------------------------
  
-# Exercício 3: Verifique o valor inicial do atributo ativo
+# Verifique o valor inicial do atributo ativo
 if (restaurante_praca.ativo): 
     print("O restaurante está ativo.")
 else: 
     print("O restaurante está inativo")
 
 # -------------------------
-# Exercício 4: Acessar Atributo de CLASSE
-# Para acessar o valor do molde, usamos o nome da Classe (Restaurante)
-categoria = Restaurante.categoria 
-# Note: o valor que 'categoria' armazena é "", que é o valor PADRÃO da Classe.
-print(f"Valor do atributo de CLASSE 'categoria': {categoria}")
-
-# -------------------------
-# Exercício 5: Alterar Atributo de INSTÂNCIA
+#Alterar Atributo de INSTÂNCIA
 # Usamos a notação de ponto no objeto específico (restaurante_praca)
 restaurante_praca.nome = "Bistrô"
 print(f"Novo nome do restaurante Praça: {restaurante_praca.nome}")
 
 # -------------------------
-# Exercício 6: Criação e Atribuição de Nova Instância
-restaurante_pizza = Restaurante ()
-restaurante_pizza.nome = "Pizza Place"
-restaurante_pizza.categoria = "Fast Food"
-
-# -------------------------
-# Exercício 7: Verifique se a categoria da instância restaurante_pizza é 'Fast Food'.
+#  Verifique se a categoria da instância restaurante_pizza é 'Fast Food'.
 if (restaurante_pizza.categoria == "Fast Food"): 
     print("A categoria é Fast Food.")
 else: 
     print("A categoria não é Fast Food")
 
 # -------------------------
-# Exercício 8: Mudança de Estado (Ativo)
+#  Mudança de Estado Restaurante (Ativo)
 restaurante_pizza.ativo = True
 if(restaurante_pizza.ativo):
     print("O status do Restaurante Pizza Place Ativo")
 else: 
     print("O status do Restaurante Pizza Place Inativo")
 
-# -------------------------
-# Exercício 9: Impressão de Múltiplos Atributos
-print(f"O restaurante {restaurante_praca.nome} é do segmento {restaurante_praca.categoria}")
-    
