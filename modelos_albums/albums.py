539e82a68134326ef87bf6944cf91c6c4fc19fa1
Alexandra Rocha <alexandrasilvarocha2824@gmail.com>
2025-11-02 16:26:39 -0300

    Meu próprio código de OO - Utilizando Classes e métodos

--- modelos_albums/albums.py
+++ modelos_albums/albums.py
@@ -5,16 +5,16 @@ class Album:
         self.nome = nome
         self.elemento = elemento
         self.ano = ano
-        self.ativo = True # Atributo de CLASSE (valor padrão)
+        self.lancado = True # Atributo de CLASSE (valor padrão)
         Album.discografia.append(self)
         
     def __str__(self):
-        return f"{self.nome} | {self.elemento}| {self.ano}"
+        return f"{self.nome} | {self.elemento} | {self.ano}"
     
     def lista_albums (): 
         print("--- Discografia ---")
         for album in Album.discografia:
-            print("album")
+            print(f"{album.nome} | {album.elemento}| {album.ano}")
             
 album_lobos = Album("Lobos", "Terra", 2018)
 album_antiheroi = Album("Anti-herói", "AR", 2019)
@@ -22,3 +22,4 @@ album_pirata = Album("Pirata","Água", 2021)
 album_super = Album("Super", "Fogo", 2023)
 album_supernova = Album("Supernova", " ", 2024)
         
+Album.lista_albums()
