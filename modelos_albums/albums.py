8a18aaece56b0f6afec917e18c3de350fedf6530
Alexandra Rocha <alexandrasilvarocha2824@gmail.com>
2025-11-09 19:49:05 -0300

    Transformando os métodos da classe em privados

--- modelos_albums/albums.py
+++ modelos_albums/albums.py
@@ -2,10 +2,10 @@ class Album:
     discografia = []
     
     def __init__(self,nome,elemento,ano):
-        self.nome = nome
-        self.elemento = elemento
-        self.ano = ano
-        self._lancado = True # Atributo de CLASSE (valor padrão)
+        self._nome = nome.upper()
+        self._elemento = elemento.title()
+        self._ano = ano
+        self._lancado = True 
         Album.discografia.append(self)
         
     def __str__(self):
@@ -13,9 +13,9 @@ class Album:
     
     def lista_albums (): 
         print("--- Discografia ---")
-        print(f"{'Nome do Album'.ljust(15)} | {'Elemento'.ljust(15)} | {'Ano'.ljust(15)} | {'Status'.ljust(15)}")
+        print(f"{'Nome do Album'.ljust(15)} | {'Elemento'.ljust(15)} | {'Ano'.ljust(15)} | {'Lançamento'.ljust(15)}")
         for album in Album.discografia:
-            print(f"{album.nome.ljust(15)} | {album.elemento.ljust(15)} | {str(album.ano).ljust(15)} | {album.lancado.ljust(15)}")
+            print(f"{album._nome.ljust(15)} | {album._elemento.ljust(15)} | {str(album._ano).ljust(15)} | {album.lancado.ljust(15)}")
             
     @property
     def lancado(self): 
